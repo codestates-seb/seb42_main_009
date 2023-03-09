666624dd8cc5d66bb47c75b8109a843dfa40971b
hunjeong <rhkrgnswjd@naver.com>
2023-03-09 13:34:32 +0900

    [feat] open api data -> mysql data save

--- pharm_palm/src/main/java/com/codestates/medicine/repository/MedicineRepository.java
+++ pharm_palm/src/main/java/com/codestates/medicine/repository/MedicineRepository.java
@@ -1,2 +1,7 @@
-package com.codestates.medicine.repository;public interface MedicineRepository {
+package com.codestates.medicine.repository;
+
+import com.codestates.medicine.entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
 }
