a229d8e0c3e2223f412e9262c8beb96e218d9eee
yjkim <k.yeojin15@gmail.com>
2023-03-13 15:19:07 +0900

    Feat: 리뷰페이지 작업중

--- client/src/components/ItemReview.js
+++ client/src/components/ItemReview.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { SmBtn } from '../styles/globalStyle';
 import { IoMdClose } from 'react-icons/io';
 import { FaPlus } from 'react-icons/fa';
 
@@ -29,6 +31,54 @@ const ReviewBtn = styled.button`
     color: #fff;
   }
 `;
+const ReviewList = styled.ul`
+  margin-top: 15px;
+`;
+const ReviewItem = styled.li`
+  position: relative;
+  width: 100%; border-radius: 6px; background: var(--palebl); padding: 20px; margin-bottom: 10px;
+  height: 120px;
+  &.review-open {
+    height: auto;
+  }
+  .btn-wrap {
+    position: absolute; right: 10px; top: 15px;
+  }
+  .btn-more {
+    position: absolute; right: 10px; bottom: 15px; color: var(--mainbl); font-weight: 600; cursor: pointer;
+  }
+`;
+const ReviewContent = styled.div`
+  display: flex; justify-content: flex-start; align-items: flex-start;
+`;
+const UserImage = styled.div`
+  width: 50px; height: 50px; border-radius: 50%;
+  flex: none; overflow: hidden; margin-top: 5px;
+  > img {width: 100%; height: 100%; object-fit: cover;}
+`;
+const UserInputs = styled.div`
+  width: calc(100% - 120px); margin-left: 10px;
+  padding-top: 10px;
+  .writedate {padding-left: 10px; color: var(--bl-2); font-size: var(--fz-sm);}
+  div {
+    width: 100%;
+    margin-top: 15px; line-height: 20px;
+    display: -webkit-box;
+    text-overflow: ellipsis;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+    ${ReviewItem}.review-open &{
+      display: block;
+      text-overflow: initial;
+      -webkit-line-clamp: initial;
+      overflow: visible;
+    }
+  }
+`;
+
+
+
 const ReviewModalBack = styled.div`
   position: fixed;
   left: 0;
@@ -45,6 +95,7 @@ const ReviewModal = styled.div`
   transform: translate(-50%, -50%);
   width: 500px;
   height: 60vh;
+  padding: 50px 20px;
   border-radius: 12px;
   background: #fff;
   > .close {
@@ -55,19 +106,96 @@ const ReviewModal = styled.div`
     cursor: pointer;
   }
 `;
+const ReviewImage = styled.div`
+  width: 100%; height: 100px;
+  border: 1px dashed var(--bl-2);
+  border-radius: 6px;
+`;
+const ReviewText = styled.textarea`
+  margin-top: 15px;
+  width: 100%; height: 130px; resize: none;
+  border: 1px solid var(--mainbl);
+  border-radius: 6px;
+  padding: 10px;
+  font-size: var(--fz-sm); line-height: 18px;
+`;
+const ReviewMedSelect = styled.div`
+  margin-top: 15px;
+  width: 100%;
+  background: var(--bl-1);
+  border-radius: 6px;
+  padding: 10px;
+  > input {
+    border-bottom: 1px solid var(--mainbl);
+  }
+  .entered-med {
+    display: flex; justify-content: flex-start; align-items: flex-start; flex-wrap: wrap;
+    margin-top: 15px;
+  }
+  p {
+    display: flex; justify-content: flex-start; align-items: center;
+    padding: 3px 4px 4px 6px; background: var(--lightbl); border-radius: 4px; margin-right: 4px;
+    cursor: pointer;
+    > span {padding-right: 4px;}
+  }
+`;
+
 
 const ItemReview = () => {
-  const [reviewOpen, setReviewOpen] = useState(false);
+  const [ reviewOpen, setReviewOpen ] = useState(false);
+  const [ reviewMedInput, setReviewMedInput ] = useState("");
+  const [ reviewStretch, setReviewStretch ] = useState(false)
   const reviewModalOpen = e => {
     e.stopPropagation();
     setReviewOpen(!reviewOpen);
   };
+  const reviewInputHandler=(e)=>{
+    const content=e.target.value
+    setReviewMedInput(content)
+  }
+  const reviewInputEnter=(e)=>{
+    if (e.key === ',') {
+      setReviewMedInput("")
+    }
+  }
+  const moreReview=()=>{
+    setReviewStretch(!reviewStretch)
+  }
+
   return (
     <ReviewWrap>
       <ReviewBtn onClick={reviewModalOpen}>
         <FaPlus />
         <span>리뷰쓰기</span>
       </ReviewBtn>
+
+      <ReviewList>
+        <ReviewItem className={reviewStretch?'review-open review':'review'}>
+          <div className='btn-wrap'>
+            <SmBtn>수정</SmBtn>
+            <SmBtn background='var(--red-1)' color='var(--red-2)' border='1px solid var(--red-2)' hoverBg='var(--red-2)' hoverColor='var(--red-1)' marginLeft='4px'>삭제</SmBtn>
+          </div>
+          <div onClick={()=>moreReview()} className='btn-more'>{reviewStretch?'닫기':'더보기'}</div>
+          <ReviewContent>
+            <UserImage>
+              <img src='https://picsum.photos/300/200' alt='user'/>
+            </UserImage>
+            <UserInputs>
+              <span className='username'>약먹기시러</span>
+              <span className='writedate'>2023-09-13</span>
+              <div>
+                Enforce onClick is accompanied by at least one of the following: onKeyUp, onKeyDown, onKeyPress. Coding for the keyboard is important for users with physical disabilities who cannot use a mouse, AT compatibility, and screenreader users. This does not apply for interactive or hidden elements.
+                Enforce onClick is accompanied by at least one of the following: onKeyUp, onKeyDown, onKeyPress. Coding for the keyboard is important for users with physical disabilities who cannot use a mouse, AT compatibility, and screenreader users. This does not apply for interactive or hidden elements.
+                Enforce onClick is accompanied by at least one of the following: onKeyUp, onKeyDown, onKeyPress. Coding for the keyboard is important for users with physical disabilities who cannot use a mouse, AT compatibility, and screenreader users. This does not apply for interactive or hidden elements.
+                Enforce onClick is accompanied by at least one of the following: onKeyUp, onKeyDown, onKeyPress. Coding for the keyboard is important for users with physical disabilities who cannot use a mouse, AT compatibility, and screenreader users. This does not apply for interactive or hidden elements.
+                Enforce onClick is accompanied by at least one of the following: onKeyUp, onKeyDown, onKeyPress. Coding for the keyboard is important for users with physical disabilities who cannot use a mouse, AT compatibility, and screenreader users. This does not apply for interactive or hidden elements.
+                Enforce onClick is accompanied by at least one of the following: onKeyUp, onKeyDown, onKeyPress. Coding for the keyboard is important for users with physical disabilities who cannot use a mouse, AT compatibility, and screenreader users. This does not apply for interactive or hidden elements.
+              </div>
+            </UserInputs>
+          </ReviewContent>
+        </ReviewItem>
+      </ReviewList>
+
       {reviewOpen ? (
         <ReviewModalBack>
           <ReviewModal>
@@ -78,6 +206,15 @@ const ItemReview = () => {
             >
               <IoMdClose />
             </button>
+            <ReviewImage><input type='file' /></ReviewImage>
+            <ReviewText maxLength={500} placeholder='복용 후기를 적어주세요.'></ReviewText>
+            <ReviewMedSelect>
+              <input type='text' value={reviewMedInput} onChange={reviewInputHandler} onKeyDown={reviewInputEnter} />
+              <div className='entered-med'>
+                <p><span>타이레놀</span><IoMdClose /></p>
+                <p><span>아타치온정50밀리그램네모정</span><IoMdClose /></p>
+              </div>
+            </ReviewMedSelect>
           </ReviewModal>
         </ReviewModalBack>
       ) : null}
