eb5a4f4d9dd5bf3c1b93dc49ce7d2933dd24b654
yjkim <k.yeojin15@gmail.com>
2023-03-15 22:49:15 +0900

    Feat: 복용약 기입 모달 페이지 진행중

--- client/src/Pages/MyPharm.js
+++ client/src/Pages/MyPharm.js
@@ -1,48 +1,20 @@
-/* eslint-disable */
-import React from 'react'
-import styled from 'styled-components'
-import Banner from '../components/Banner'
-import MyPharmInput from '../components/MyPharmInput';
-import MyPharmList from '../components/MyPharmList';
-
+import React, { useState } from 'react';
+import Banner from '../components/Banner';
+import MyPharmModal from '../components/MyPharmModal';
 
 const MyPharm = () => {
+  const [modalOpen, setModalOpen] = useState(true);
   return (
     <>
-      <Banner><div>복용 약 관리</div></Banner>
-      <div className='bodywrap'>
-        <MyPharmInput/>
-        <MyPharmList/>
-        {/* <MyPharmWrap>
-          <FieldSet>
-            <FieldTitle>현재 복용하고 있는 의약품의 이름을 입력해 주세요.</FieldTitle>
-            <InputBox>
-              <label htmlFor='medicine_name'>약품명</label>
-              <TextInput type='text' id='medicine_name' />
-            </InputBox>
-          </FieldSet>
-          <FieldSet>
-            <FieldTitle>현재 복용량과 복용 기간을 입력해 주세요.</FieldTitle>
-            <InputBox>
-              <label htmlFor='medicine_day'>1일 복용 횟수</label>
-              <TextInput type='text' id='medicine_day' />
-            </InputBox>
-            <InputBox>
-              <label htmlFor='medicine_count'>1회 복용량</label>
-              <TextInput type='text' id='medicine_count' />
-            </InputBox>
-            <InputBox>
-              <label htmlFor='medicine_time'>복용 시간</label>
-              <DateInput>
-                <TimeInput/>
-              </DateInput>
-              <button aria-label='button'><FaPlus/></button>
-            </InputBox>
-          </FieldSet>
-        </MyPharmWrap> */}
+      <Banner>
+        <div>복용 약 관리</div>
+      </Banner>
+      <div className="bodywrap">
+        <button>추가하기</button>
+        {modalOpen ? <MyPharmModal setModalOpen={setModalOpen} /> : null}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MyPharm
+export default MyPharm;
