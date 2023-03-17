2286329309932b9a6d2cce87e89df1f5964b28cf
rayul <prettykiki92@gmail.com>
2023-03-17 18:55:25 +0900

    feat : 질병 이름으로 검색, 전체 검색 기능 구현

--- pharm_palm/src/main/java/com/codestates/disease/controller/DiseaseController.java
+++ pharm_palm/src/main/java/com/codestates/disease/controller/DiseaseController.java
@@ -1,12 +1,19 @@
 package com.codestates.disease.controller;
 
+import com.codestates.disease.dto.DiseaseResponseDto;
 import com.codestates.disease.service.DiseaseService;
+import com.codestates.dto.MultiResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
+import java.util.List;
+
 @RestController
-@RequestMapping("/pp/disease")
+@RequestMapping("/pp/diseases")
 @Slf4j
 public class DiseaseController {
 
@@ -17,13 +24,26 @@ public class DiseaseController {
     }
 
     @GetMapping("/name")
-    public ResponseEntity getDisease() {
-        return null;
+    public ResponseEntity getDisease(@RequestParam(required = false) String diseaseName) {
+        if (diseaseName == null || diseaseName.isEmpty()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        Page<DiseaseResponseDto> diseaseResponses = diseaseService.findByDiseaseNameLike(diseaseName);
+        List<DiseaseResponseDto> diseaseList = diseaseResponses.getContent();
+
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(diseaseList, diseaseResponses), HttpStatus.OK);
+
     }
 
     @GetMapping
-    public ResponseEntity getDiseases() {
-        return null;
+    public ResponseEntity getDiseases(@Positive @RequestParam int page,
+                                      @Positive @RequestParam int size) {
+        Page<DiseaseResponseDto> diseaseResponses = diseaseService.findDiseases(page -1,size);
+        List<DiseaseResponseDto> diseaseList = diseaseResponses.getContent();
+
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(diseaseList, diseaseResponses), HttpStatus.OK);
     }
 
 }
--- pharm_palm/src/main/java/com/codestates/disease/service/DiseaseService.java
+++ pharm_palm/src/main/java/com/codestates/disease/service/DiseaseService.java
@@ -1,14 +1,22 @@
 package com.codestates.disease.service;
 
 import com.codestates.disease.DiseasesData;
+import com.codestates.disease.dto.DiseaseResponseDto;
 import com.codestates.disease.entity.Disease;
 import com.codestates.disease.mapper.DiseaseMapper;
 import com.codestates.disease.repository.DiseaseRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DiseaseService {
@@ -21,18 +29,36 @@ public class DiseaseService {
         this.mapper = mapper;
     }
 
-//    @PostConstruct
-//    public void defaultDiseases(DiseasesData diseasesData) {
-//        List<Disease> diseases = diseasesData.getDiseaseList();
-//        diseaseRepository.saveAll(diseases);
+    public Page<DiseaseResponseDto> findByDiseaseNameLike (String diseaseName) {
+        List<Disease> diseases = diseaseRepository.findByDiseaseNameLike("%" + diseaseName + "%")
+                .stream()
+                .sorted(Comparator.comparing(Disease::getDiseaseName))
+                .collect(Collectors.toList());
+        List<DiseaseResponseDto> diseaseResponses = mapper.diseasesToResponseDto(diseases);
+
+        return new PageImpl<>(diseaseResponses);
+
+    }
+
+//    public Page<DiseaseResponseDto> findDiseases(int page, int size) {
+//        Page<Disease> diseases = diseaseRepository.findAll(PageRequest.of(page, size,
+//                Sort.by("diseaseId").descending()));
+//
+//        return mapper.diseasesToResponseDtoPage(diseases);
 //    }
+    public Page<DiseaseResponseDto> findDiseases(int page, int size) {
+        List<Disease> diseases = diseaseRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Disease::getDiseaseName))
+                .collect(Collectors.toList());
+        List<DiseaseResponseDto> diseaseResponses = mapper.diseasesToResponseDto(diseases);
 
+        int startIndex = page * size;
+        int endIndex = Math.min(startIndex + size, diseaseResponses.size());
 
-    public Disease findDisease () {
-        return null;
+        return new PageImpl<>(diseaseResponses.subList(startIndex, endIndex), PageRequest.of(page, size), diseaseResponses.size());
     }
 
-    public List<Disease> findDiseases() {
-        return null;
-    }
+
+
 }
