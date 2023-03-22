4b353b79d9a4d6ac3f77f8cf6aa269a3c7b7cd46
rayul <prettykiki92@gmail.com>
2023-03-22 15:28:25 +0900

    feat : logout api추가

--- pharm_palm/src/main/java/com/codestates/auth/controller/LogoutController.java
+++ pharm_palm/src/main/java/com/codestates/auth/controller/LogoutController.java
@@ -1,7 +1,12 @@
 package com.codestates.auth.controller;
 
+import com.codestates.auth.refreshToken.repository.RefreshTokenRepository;
+import com.codestates.auth.refreshToken.service.RefreshTokenService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +16,10 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/pp/logout")
 @Slf4j
 public class LogoutController {
+    @PostMapping
+    public ResponseEntity logout () throws Exception {
+        log.info("# 사용자가 로그아웃 했습니다.");
 
-    @GetMapping
-    public String logout (HttpServletRequest request) throws Exception {
-        return null;
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
