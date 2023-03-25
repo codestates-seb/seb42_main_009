d6ec5675f168c212b7c9deb360d76b779f3ca4ff
yjkim <k.yeojin15@gmail.com>
2023-03-25 19:00:08 +0900

    fix scroll

--- client/src/Pages/List.js
+++ client/src/Pages/List.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import ReactPaginate from 'react-paginate';
-import { useNavigate } from 'react-router-dom';
+import { json, useNavigate } from 'react-router-dom';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import {
   ContentList,
@@ -30,6 +30,7 @@ const List = () => {
   // 1. currentPage 초기값은 0으로 설정
   const [currentPage, setCurrentPage] = useState(1);
   const [totalLength, setTotalLength] = useState(0);
+  const [totalPageCount, setTotalPageCount] = useState(0);
 
   const PER_PAGE = 16;
   const pageCount = Math.ceil(totalLength / PER_PAGE);
@@ -41,47 +42,53 @@ const List = () => {
     e.target.src = '/pharmpalm.png';
   };
 
-  useEffect(() => {
+  useEffect(() => async () => {
     if (searchText === '') {
-      axios
+      await axios
         .get(`${URI}/pp/medicines?page=${currentPage}&size=${PER_PAGE}`)
         .then(res => {
           console.log(res);
           setItemList(res.data.data);
           setTotalLength(res.data.pageInfo.totalElements);
+          setTotalPageCount(res.data.pageInfo.totalPages);
+          setLoading(!(page === res.data.pageInfo.totalPages));
+          console.log('dfasdfas >', res.data.pageInfo.totalPages)
         })
         .catch(err => {
           console.log(err);
         });
-    } else {
-      console.log('검색 성공');
-      console.log(searchText);
-      axios
+      } else {
+        console.log('검색 성공');
+        console.log(searchText);
+        await axios
         .get(
           `${URI}/pp/medicines/${searchSelected}?${searchApi}=${searchText}&page=${currentPage}&size=${PER_PAGE}`,
-        )
-        .then(res => {
-          // 검색한 아이템이 없으면, 빈 배열 출력
-          if (!res.data) {
+          )
+          .then(res => {
+            // 검색한 아이템이 없으면, 빈 배열 출력
+            if (!res.data) {
             setItemList([]);
             setTotalLength(0);
+            setTotalPageCount(0);
           }
           // 검색한 아이템이 있으면, 해당 아이템 출력
           else {
             setItemList(res.data.data);
             setTotalLength(res.data.pageInfo.totalElements);
+            setTotalPageCount(res.data.pageInfo.totalPages);
+            setLoading(!(page === res.data.pageInfo.totalPages));
           }
         })
         .catch(err => {
           console.log(err);
         });
-    }
-  }, [currentPage, searchText, searchSelected]);
-
-  const itemOnClickHandler = medicineId => {
-    navigate(`/item/${medicineId}`);
-    // window.location.reload();
-  };
+      }
+    }, [currentPage, searchText, searchSelected]);
+    
+    const itemOnClickHandler = medicineId => {
+      navigate(`/item/${medicineId}`);
+      // window.location.reload();
+    };
 
 
   // 무한스크롤
@@ -90,49 +97,74 @@ const List = () => {
   const [loading, setLoading] = useState(false);
   const pageEnd = useRef()
   const loadMore=()=>{
-    setPage(prev=>prev+1)
-  }
-
-  useEffect(()=>{
-    const fetchPins = async page => {
-      if(searchText===''){
-        axios
-        .get(`${URI}/pp/medicines?page=${page}&size=6`)
-        .then(res=>{
-          setPins(prev => [...prev, ...res.data.data]);
-          console.log('성공')
-        })
-        .catch(err=>console.log(err))
-      }else{
-        axios
-        .get(`${URI}/pp/medicines/${searchSelected}?${searchApi}=${searchText}&page=${page}&size=6`)
-        .then(res=>{
-          setPage(0);
-          console.log('검색어입력했을때성공했는지')
-          if(!res.data){
-            setPins([])
-          }else{
-            setPins(prev => [...prev, ...res.data.data]);
-          }
-        })
-        .catch(err=>console.log(err))
-      }
-      setLoading(true);
+    console.log (totalPageCount)
+    console.log (page)
+    if (totalPageCount !== 0 && totalPageCount > page) {
+      setPage(prev=>prev+1)
     }
+  }
+  const submitPageHandler=()=>{
+    setCurrentPage(1)
+    setPage(1)
+  }
+  useEffect(()=>{
     fetchPins(page)
   },[page,searchText,searchSelected])
-	useEffect(() => {
-    if (loading) {
-      const observer = new IntersectionObserver(
-        entries => {
-          if (entries[0].isIntersecting) {
-            loadMore();
+
+  const fetchPins = async page => {
+    if(searchText===''){
+      console.log('aaaa >>> ', page)
+      await axios
+      .get(`${URI}/pp/medicines?page=${page}&size=6`)
+      .then(res=>{
+        setTotalPageCount(res.data.pageInfo.totalPages);
+        if(page === 1) {
+          setPins(prev => [...res.data.data]);
+        } else {
+          setPins(prev => [...prev, ...res.data.data]);
+        }
+        console.log('성공')
+      })
+      .catch(err=>console.log(err))
+    }else{
+      console.log('aaaa >>> ', page)
+      console.log('aaaa >>> ', totalPageCount)
+      await axios
+      .get(`${URI}/pp/medicines/${searchSelected}?${searchApi}=${searchText}&page=${page}&size=6`)
+      .then(res=>{
+        console.log('검색어입력했을때성공했는지')
+        if(!res.data){
+          setPins([])
+        }else{
+          console.log('fetchpin>>>', res.data.pageInfo.totalPages)
+          setTotalPageCount(res.data.pageInfo.totalPages);
+          if(page === 1) {
+            setPins(prev => [...res.data.data]);
+          } else {
+            setPins(prev => [...prev, ...res.data.data]);
           }
-        },
-        { threshold: 1 }
-      );
+
+          setLoading(!(page === res.data.pageInfo.totalPages));
+        }
+      })
+      .catch(err=>console.log(err))
+    }
+    // setLoading(true);
+  }
+
+	useEffect(() => {
+    let observer;
+    if (loading) {
+      observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+          loadMore();
+        }
+      }, {
+        threshold: 0.4,
+      });
       observer.observe(pageEnd.current);
     }
+    return () => observer && observer.disconnect();
   }, [loading]);
 
 
@@ -148,7 +180,7 @@ const List = () => {
         <div>의약품조회</div>
       </Banner>
       <div className="bodywrap">
-        <Search />
+        <Search submitPageHandler={submitPageHandler} />
         <ContentList className='none-mo'>
           {itemList.map((item, idx) => (
             <ContentBox
@@ -205,6 +237,7 @@ const List = () => {
             // containerClassName="pagination"
             subContainerClassName=""
             activeClassName="active"
+            forcePage={currentPage-1}
           />
         </Pagination>
       </div>
